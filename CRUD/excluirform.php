41c90ef50e829c824674813cab45777f319f9aab
lucasbalbino09 <balbino2409@gmail.com>
2022-09-25 16:37:39 -0300

    Atualizando na main

---
+++ CRUD/excluirform.php
@@ -0,0 +1,51 @@
+<html>
+        <head>
+            <title>Excluir o Administrador</title>
+        </head>
+        <body>
+            <h1>Excluir o Administrador</h1>
+            <br>
+
+<?php
+<<<<<<< Updated upstream
+$mysqlhostname= "144.22.244.104";
+$mysqlport="3306";
+$mysqlusername="Bravo4Fun";
+$mysqlpassword="Bravo4Fun";
+$mysqldatabase="Bravo4Fun";
+=======
+ $mysqlhostname= "144.22.244.104";
+ $mysqlport="3306";
+ $mysqlusername="Bravo4Fun";
+ $mysqlpassword="Bravo4Fun";
+ $mysqldatabase="Bravo4Fun";
+>>>>>>> Stashed changes
+
+//mostra string de conexao ao MySql
+$dsn='mysql:host=' . $mysqlhostname . ";dbname=" . $mysqldatabase . ';port=' . $mysqlport;
+$pdo= new PDO($dsn, $mysqlusername, $mysqlpassword);        
+        
+//coleta os dados do administrador
+$id = $_GET["id"];
+
+//Realiza uma Query SQL para buscar o administrador que tenha o email e a senha passado pelo usuario
+$admin= $pdo->query("SELECT * FROM ADMINISTRADOR WHERE ADM_ID=" . $id)->fetch();
+
+//Se o retorno for vazio (0), então a senha ou email estao incorretos
+$nome= $admin["ADM_NOME"];
+$email= $admin["ADM_EMAIL"];
+?>
+            <Form Action="excluirprocessamento.php" method="POST">
+                <input type="hidden" name="id" value="<?php echo $id ?>">
+                <br>
+                Nome : 
+                <input type="text" name="nome" value="<?php echo $nome ?>">
+                <br>
+                Email : 
+                <input type="text" name="email" value="<?php echo $email ?>">
+                <br>
+                <input type="submit" value="Enviar"> 
+            </Form>
+        </body>
+</html>        
+	
---
+++ CRUD/listaradmins.php
@@ -0,0 +1,75 @@
+    <?php
+    $mysqlhostname= "144.22.244.104";
+    $mysqlport="3306";
+    $mysqlusername="Bravo4Fun";
+    $mysqlpassword="Bravo4Fun";
+    $mysqldatabase="Bravo4Fun";
+
+    //mostra string de conexao ao MySql
+    $dsn='mysql:host=' . $mysqlhostname . ";dbname=" . $mysqldatabase . ';port=' . $mysqlport;
+    $pdo= new PDO($dsn, $mysqlusername, $mysqlpassword);
+
+    $cmd= $pdo->query("SELECT * FROM ADMINISTRADOR");   
+    
+    $admins = $cmd->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+    <html>        
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="ListadeAdmin.css">
+        <title>Listar os Administradores</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul class="nav justify-content nav">
+                    <li class="nav-item">
+                    <img src="logoBravo.png" alt="LogoMarca" class="imagem">
+                    </li>
+                    <li class="nav-item">
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <h1>Lista de Administradores</h1>
+       
+
+    <?php
+    $mysqlhostname= "144.22.244.104";
+    $mysqlport="3306";
+    $mysqlusername="Bravo4Fun";
+    $mysqlpassword="Bravo4Fun";
+    $mysqldatabase="Bravo4Fun";
+    ?>
+
+
+        <table border="1" class="table">
+            <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Email</th>
+                <th scope="col">Senha</th>
+                <th scope="col">Atualização</th>
+                <th scope="col">Exclusão</th>            
+            </tr>
+            </thead>
+        <tr>
+    <?php
+        foreach ($admins as $val) {
+            echo "<tr>";
+            echo "<td>{$val("ADM_ID")}</td>";
+            echo "<td>{$val("ADM_NOME")}</td>";                    
+            echo "<td>{$val("ADM_EMAIL")}</td>";           
+            echo "<td>{$val("ADM_SENHA")}</td>/";
+            echo "<td>{$val("ADM_SENHA")}</td>/";
+            echo "<td><a href=\"atualizarform.php?id={$val("ADM_ID")}\"> Atualizar</a></td>";           
+            echo "<td><a href=\"excluirform.php?id={$val("ADM_ID")}\">Excluir</a></td>";
+            echo"</tr>";
+        }
+    ?>
+        </table>
+        <br>
+        <a href="CRUD/index.html"><button type="button" class="btn btn-primary">Voltar para o Indice</button></a>    
+    </body>
+    </html>
