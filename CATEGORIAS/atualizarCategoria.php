f5dabafb7f4ffd2c50eaf1db7ae6b77ab796f02a
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2022-10-08 23:21:33 -0300

    Lista de categorias e formulario

--- CATEGORIAS/atualizarCategoria.php
+++ CATEGORIAS/atualizarCategoria.php
@@ -5,11 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">   
-        <title>Adicionando novos Administradores</title>
+        <title>Atualização de Categorias</title>
         <link rel="stylesheet" href="style.css" type="text/css">
     </head>
     <body>
-        <h1>Atualizado</h1>          
+        <h1>Atualização de Categorias</h1>          
 
                
             <?php
@@ -26,21 +26,21 @@
                 $pdo= new PDO($dsn, $mysqlusername, $mysqlpassword);
 
                 //Captura os valores das variaveis
-                $nome= $_POST["nome"];
-                $email= $_POST["email"];
-                $senha= $_POST["senha"];
+                $nomeId= $_POST["CATEGORIA_ID"];
+                $nome= $_POST["CATEGORIA_NOME"];
+                $desc= $_POST["CATEGORIA_DESC"];
 
                 // requisição do cliente!
                 $id = $_POST["id"];
 
                 //Monta o comando de update!
-                $cmdtext= "UPDATE ADMINISTRADOR set ADM_NOME = '$nome', ADM_EMAIL = '$email', ADM_SENHA = '$senha' where ADM_ID=  $id";
+                $cmdtext= "UPDATE CATEGORIA set CATEGORIA_NOME = '$nome', CATEGORIA_DESC = '$desc' where CATEGORIA_ID=  $nomeid";
                 $cmd = $pdo->prepare($cmdtext);
 
                 //Executa o comando e verifica se teve sucesso
                 $status = $cmd->execute();
                 if($status){
-                    echo "<script>Alert('Adm criado com sucesso')</script>";
+                    echo "<script>Alert('Categoria/descrição atualizadas')</script>";
                 }
                 else
                 {
@@ -48,7 +48,7 @@
                 }     
                 
             ?>
-            <a href="listaradmins.php">Voltar para a Pagina de Lista</a>
+            <a href="listaCategoria.php">Voltar para a Pagina de Lista</a>
         
     </body>
 </html>
