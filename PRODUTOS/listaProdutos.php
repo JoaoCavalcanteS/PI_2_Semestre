8efc1b8456d7fae7e12588a6dd6fa8d16708ffb5
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2022-10-09 22:01:29 -0300

    Pasta Produtos

--- PRODUTOS/listaProdutos.php
+++ PRODUTOS/listaProdutos.php
@@ -1,4 +1,4 @@
-    <html>        
+<html>        
     <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="ListadeAdmin.css">
@@ -44,10 +44,11 @@
         <tr>
             <th>Id</th>
             <th>Nome</th>
-            <th>Email</th>
-            <th>Senha</th>
-            <th>Atualizacao</th>
-            <th>Exclusao</th>            
+            <th>Descrição</th>
+            <th>Preço</th>
+            <th>Desconto</th>
+            <th>Categoria ID</th>            
+
         </tr>
 <?php
 
